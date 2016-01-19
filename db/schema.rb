21fc407f56984e6b65c75f4b356acc031a7dad7f
Zazou3448 <elsa.velay@gmail.com>
2016-01-19 15:25:40 +0000

    Activities

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118105139) do
+ActiveRecord::Schema.define(version: 20160119151410) do
 
   create_table "activities", force: :cascade do |t|
-    t.integer  "artisan_id"
     t.string   "job"
     t.string   "services"
     t.string   "description"
